659c712efff34929fa190ff69cbf692e4ee9e32a
Sophia Riela <sophiarielaprofissional@gmail.com>
2026-04-03 16:06:53 -0300

    first deploy

--- app.py
+++ app.py
@@ -7,48 +7,67 @@ PRODUTOS = [
         "id": 1,
         "nome": "Vestido Bahia Blue",
         "preco": 200.00,
-        "descricao": "Crochê halter neck em azul royal.",
-        "imagem": "https://drive.google.com/thumbnail?id=1MLL3YjLFlP8EsQ-mR9-nQq0kf8iNn1Pg&sz=w1000",
+        "descricao": "Vestido halter neck em crochê artesanal, linha 100% algodão azul royal vibrante. Amarração no pescoço, cintura marcada, barra com babado em ponto pipoca. Comprimento mini, trama fechada. Forro opcional. Lavar à mão, secar à sombra.",
+        "imagem": "/static/imagens/vestido-bahia-blue.jpg",
         "categoria": "vestidos"
     },
     {
         "id": 2,
-        "nome": "Vestido Barra Coral",
-        "preco": 110.00,
-        "descricao": "Tomara que caia com twist frontal e recortes laterais. Cor de Caribe.",
-        "imagem": "https://drive.google.com/thumbnail?id=1mpCBdACvpZmz9HE4YgGIrEfIrqQe9Olg&sz=w1000",
-        "categoria": "vestidos"
+        "nome": "Conjunto Flor de Maio",
+        "preco": 190.00,
+        "descricao": "Conjunto composto por top e saia mini em crochê artesanal, linha 100% algodão off-white com detalhes vinho/rosa/. Top com aplicação de orquídea e tiras reguláveis. Saia com cordão, miçangas vermelhas e babado. Disponível separadamente ou conjunto. Lavar à mão, secar à sombra.",
+        "imagem": "/static/imagens/conjunto-flor-de-maio.png",
+        "categoria": "conjuntos"
     },
     {
         "id": 3,
-        "nome": "Conjunto Pôr do Sol Tropical",
-        "preco": 200.00,
-        "descricao": "Duas peças off-white com conchas douradas. Delicado e sofisticado.",
-        "imagem": "https://drive.google.com/thumbnail?id=1pSxl53vvtaBd4Czn1_rJvFsPDopsgDNj&sz=w1000",
-        "categoria": "conjuntos"
+        "nome": "Vestido Nevoa",
+        "preco": 300.00,
+        "descricao": "Vestido curto em crochê artesanal, linha 100% algodão off-white. Halter neck com costas abertas, babado na barra. Trama aberta estilo beach/festival, comprimento mini. Forro opcional. Lavar à mão, secar à sombra.",
+        "imagem": "/static/imagens/vestido-nevoa.png",
+        "categoria": "vestidos"
     },
     {
         "id": 4,
         "nome": "Vestido Areia Dourada",
         "preco": 300.00,
-        "descricao": "Tom de areia, textura rica, caimento que abraça.",
-        "imagem": "https://drive.google.com/thumbnail?id=1Bz2d_SlRsQQqAHD712JmOYTXSNJ82N6t&sz=w1000",
+        "descricao": "Vestido assimétrico ombro único em crochê artesanal, linha 100% algodão off-white. Alça larga com detalhe vazado lateral, faixa floral assimétrica. Comprimento médio, barra assimétrica. Trama aberta estilo beach. Forro opcional. Lavar à mão, secar à sombra.",
+        "imagem": "/static/imagens/vestido-areia-dourada.png",
         "categoria": "vestidos"
     },
     {
         "id": 5,
-        "nome": "Conjunto Maré Alta",
-        "preco": 270.00,
-        "descricao": "Franjas que balançam, duas peças, infinitas combinações.",
-        "imagem": "https://drive.google.com/thumbnail?id=1Iy6g9-K4C_JP1Y46tEtqucZsOZaJKDsw&sz=w1000",
-        "categoria": "conjuntos"
+        "nome": "Top Terra Preta",
+        "preco": 70.00,
+        "descricao": "Top triangular halter neck em crochê artesanal, 100% poliamida na cor chocolate/marrom. Amarração no pescoço e costas, aplicações florais com miçangas em tons terrosos. Tiras reguláveis com miçangas de madeira. Lavar à mão, secar à sombra.",
+        "imagem": "/static/imagens/top-terra-preta.png",
+        "categoria": "tops"
     },
     {
-        "id": 6,
+
+	"id": 6,
+	"nome": "Top Orquídea",
+	"preco": 90.00,
+	"descricao": "Top em crochê artesanal, linha 100% algodão off-white com aplicação de orquídea multicolorida (vinho, rosa). Amarração no pescoço e costas, tiras reguláveis com miçangas. Trama aberta estilo beach/festival. Lavar à mão, secar à sombra.",
+	"imagem": "/static/imagens/top-orquidea.png",
+	"categoria": "tops"
+
+   },
+   {
+	"id": 7,
+	"nome": "Saia Piteira",
+	"preco": 100.00,
+	"descricao": "Saia mini em crochê artesanal, linha 100% algodão off-white. Cintura alta com cordão regulável e miçangas vermelhas, barra com babado em ponto leque. Elástico interno para melhor ajuste. Lavar à mão, secar à sombra.",
+	"imagem": "/static/imagens/saia-piteira.png",
+	"categoria": "saias & shorts"
+
+    },
+    {
+        "id": 8,
         "nome": "Vestido Estrela do Mar",
         "preco": 270.00,
-        "descricao": "Estrelinhas bordadas à mão sobre crochê off-white.",
-        "imagem": "https://drive.google.com/thumbnail?id=1Myf3qW6BobigvUvpa7vVJM1tUA25HWUF&sz=w1000",
+        "descricao": "Vestido em crochê artesanal, linha 100% algodão branca com aplicações de estrelas-do-mar coloridas (azul, rosa, dourado). Alças finas, barra com babado rosa em ponto leque. Cintura marcada, trama vazada. Forro opcional. Lavar à mão, secar à sombra..",
+	"imagem": "/static/imagens/vestido-estrela-do-mar.png",
         "categoria": "vestidos"
     }
 ]
@@ -59,3 +78,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
