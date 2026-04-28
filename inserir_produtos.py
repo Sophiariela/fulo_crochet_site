519368416105bafe491a2bdc4ced78225d02c33f
Sophia Riela <sophiarielaprofissional@gmail.com>
2026-04-28 17:52:40 -0300

    Fix: remover credenciais expostas

--- inserir_produtos.py
+++ inserir_produtos.py
@@ -1,6 +1,9 @@
 import psycopg2
 
-conn = psycopg2.connect("postgresql://fulo_db_user:uBnTaAWuT8V8nDZjIHEynM2VtaUx7PIx@dpg-d7ogqj9f9bms73dmv080-a.ohio-postgres.render.com/fulo_db")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+conn = psycopg2.connect(os.getenv('DATABASE_URL'))
 cur = conn.cursor()
 
 produtos = [
